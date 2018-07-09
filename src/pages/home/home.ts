b3b2f1faec8a97d7b1cf7731ec7e046e37d01a21
ORIENTSOFTWARE\nthung <robert.orient@vtnz.co.nz>
2018-07-09 13:33:43 +0700

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,34 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private pos1: number = 0;
+  private pos2: number = 0;
+  private pos3: number = 0;
+  private pos4: number = 0;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  public touchstart($event) {
+    const touch = $event.changedTouches[0];
+    this.pos3 = touch.clientX;
+    this.pos4 = touch.clientY;
+  }
+
+  public touchmove($event) {
+    $event.preventDefault();
+
+    const touch = $event.changedTouches[0];
+    const target = touch.target;
+    // calculate the new cursor position:
+    this.pos1 = this.pos3 - touch.clientX;
+    this.pos2 = this.pos4 - touch.clientY;
+    this.pos3 = touch.clientX;
+    this.pos4 = touch.clientY;
+
+    // set the element's new position:
+    target.style.top = `${target.offsetTop - this.pos2}px`;
+    target.style.left = `${target.offsetLeft - this.pos1}px`;
+  }
 }
